b6bb1d868cb62acf967a47ea2ad124878944bc4c
raash009 <raash009@outlook.com>
2014-05-08 14:24:17 +0530

    test

---
+++ RashidGitWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RashidGitWebsite.Startup))]
+namespace RashidGitWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
